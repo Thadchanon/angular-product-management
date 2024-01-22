69538b2022351aa5eff1a5d4be49bb680182ec59
Thadchanon <thadchanonjirarat@gmail.com>
2024-01-22 17:56:06 +0700

    fix: change json-server to localStorage

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,26 +1,70 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  constructor(private _http: HttpClient) {}
+  private localStorageKey = 'productData';
+
+  private getProductsFromLocalStorage(): any[] {
+    const data = localStorage.getItem(this.localStorageKey);
+    return data ? JSON.parse(data) : [];
+  }
+
+  private setProductsToLocalStorage(products: any[]): void {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(products));
+  }
 
   addProduct(data: any): Observable<any> {
-    return this._http.post('http://localhost:3000/products', data);
+    try {
+      const products = this.getProductsFromLocalStorage();
+      products.push(data);
+      this.setProductsToLocalStorage(products);
+      return of(data);
+    } catch (error) {
+      throw new Error('Cannot add product');
+    }
   }
 
   updateProduct(id: string, data: any): Observable<any> {
-    return this._http.put(`http://localhost:3000/products/${id}`, data);
+    try {
+      const products = this.getProductsFromLocalStorage();
+      const index = products.findIndex((product) => product.id === id);
+      if (index !== -1) {
+        products[index] = { ...products[index], ...data };
+        this.setProductsToLocalStorage(products);
+        return of(products[index]);
+      } else {
+        throw new Error(`Product with ID ${id} not found`);
+      }
+    } catch (error) {
+      throw new Error('Cannot update product');
+    }
   }
 
   getProductList(): Observable<any> {
-    return this._http.get('http://localhost:3000/products');
+    try {
+      const products = this.getProductsFromLocalStorage();
+      return of(products);
+    } catch (error) {
+      throw new Error('Cannot get product list');
+    }
   }
 
   deleteProduct(id: string): Observable<any> {
-    return this._http.delete(`http://localhost:3000/products/${id}`);
+    try {
+      const products = this.getProductsFromLocalStorage();
+      const index = products.findIndex((product) => product.id === id);
+      if (index !== -1) {
+        const deletedProduct = products.splice(index, 1)[0];
+        this.setProductsToLocalStorage(products);
+        return of(deletedProduct);
+      } else {
+        throw new Error(`Product with ID ${id} not found`);
+      }
+    } catch (error) {
+      throw new Error('Cannot delete product list');
+    }
   }
 }
